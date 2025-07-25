939b7d062b5eb5d135d089fffe2f87e0ec97e98e
Micael Fone <micaelfone0@gmail.com>
2025-07-25 08:09:25 -0300

    Adicionando arquivos com o arrays de projetos

---
+++ src/docs/projetosDescricao.ts
@@ -0,0 +1,39 @@
+
+export  const projetosDescricao = [
+   {
+        title: "Frontend Questionário",
+        description: "A sistema de questionário desenvolvido com React e Bootstrap, permitindo a criação ,alteração e exclusão de questionários. Recomenda usar o projeto Backend Questionário para funcionalidade completa.",
+        imageUrl: "/images/perguntamodelo.png",
+        link: "https://github.com/MIcaelFone/Backend-questionario"
+    },
+    {
+        title: "Backend Questionário",
+        description: "Uma API desenvolvida com Node.js e Express, que permite criar, editar e excluir questionários, além de registrar e exibir a contagem de votos.",
+        imageUrl: "/images/api-clinica-medica.pn",
+        link: "https://github.com/MIcaelFone/Microservico-livro"
+    },
+    {
+        title: "API Clínica Médica",
+        description: "Uma API desenvolvida Spring Boot, que permite gerenciar consultas médicas, pacientes, agendamentos e prontuários. Utiliza banco de dados MySQL e o Maven para gerenciamento de dependências, Swagger para documentação.",
+        imageUrl: "/images/api-clinica-medica.png",
+        link: "https://github.com/MIcaelFone/API-Clinica-Medica"
+    },
+    {
+        title:"Clothes Shop",
+        description: "Uma loja de roupas online desenvolvida com React, Node.js e Express. Permite aos usuários navegar por produtos, adicionar itens ao carrinho e realizar compras, além de gerenciar produtos e usuários. Esse projeto foi desenvolvido em conjunto com Thiago Luz",
+        imageUrl: "/images/clothes-shop.png",
+        link: "https://github.com/MIcaelFone/Clothes-Shop"
+    },
+    {
+        title: "Microserviço Livro",
+        description: "Um microserviço desenvolvido com Spring Boot, que permite gerenciar livros. Utiliza banco de dados MySQL e o Maven para gerenciamento de dependências, Swagger para documentação. Esse projeto foi desenvolvido em conjunto com Heron Ricardo e Rafael Felipe.",
+        imageUrl: "/images/microservico-livro.png",
+        link: "https://github.com/MIcaelFone/Microservico-livro"
+    }, 
+    {
+        title: "Alura Newsletter",
+        description: "Uma landing page de newsletter, desenvolvida durante o curso da Alura, utilizando HTML e Tailwind CSS. Para a captura de e-mails de usuários interessados em receber novidades.",
+        imageUrl: "/images/alura-newsletter.png",
+        link: "https://github.com/MIcaelFone/AluraNewsletter"
+    }
+]
